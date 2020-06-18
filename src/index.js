5f4c174cc5683b2765f32915d6b4a28e8e8a1c95
Alex Bianchi <alexssandro.bianchi@intelltech.com.br>
2020-06-17 22:07:01 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'uikit/dist/css/uikit-rtl.min.css'
+import "rbx/index.css";
+import 'antd/dist/antd.css'
+
+ReactDOM.render(<App />, document.getElementById('root'));
